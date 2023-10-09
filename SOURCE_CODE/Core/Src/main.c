4e5848186820ae8ea94df57183be6c807ed8ff76
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2023-10-09 12:57:21 +0700

    done ex5

--- SOURCE_CODE/Core/Src/main.c
+++ SOURCE_CODE/Core/Src/main.c
@@ -252,10 +252,14 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setTimer1(100);
-  setTimer2(25);
-  int status = 0;
-  state(status);
+  int hour = 15, minute = 8, second = 50;
+  void updateClockBuffer()
+  {
+	  led_buffer[0] = hour / 10;
+	  led_buffer[1] = hour % 10;
+	  led_buffer[2] = minute / 10;
+	  led_buffer[3] = minute % 10;
+  }
   while (1)
   {
 	  if (timer1_flag == 1)

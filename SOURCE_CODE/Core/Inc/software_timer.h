8ae4f84e4c8c913ad95c0b28fa325be70b99a3ef
CUCCUTTHANBI\tango <tangocnam23@gmail.com>
2023-10-09 10:15:09 +0700

    init ex1

---
+++ SOURCE_CODE/Core/Inc/software_timer.h
@@ -0,0 +1,16 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Oct 9, 2023
+ *      Author: tango
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer1_flag;
+
+void setTimer1(int duration);
+void timerRun();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
---
+++ SOURCE_CODE/Core/Src/software_timer.c
@@ -0,0 +1,28 @@
+/*
+ * software_timer.c
+ *
+ *  Created on: Oct 9, 2023
+ *      Author: tango
+ */
+
+#include "software_timer.h"
+
+int timer1_counter = 0;
+int timer1_flag = 0;
+
+void setTimer1(int duration)
+{
+	timer1_counter = duration;
+	timer1_flag = 0;
+}
+void timerRun()
+{
+	if (timer1_counter > 0)
+	{
+		timer1_counter--;
+		if (timer1_counter <= 0)
+		{
+			timer1_flag = 1;
+		}
+	}
+}
